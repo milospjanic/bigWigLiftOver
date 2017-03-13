5d9dfba007d2edac741bd649d470a6d0cdc77678
Milos Pjanic <pjanic.milos@gmail.com>
2017-03-13 14:50:37 -0700

    Update bigWigLiftOver.sh

--- bigWigLiftOver.sh
+++ bigWigLiftOver.sh
@@ -2,11 +2,12 @@
 
 #!/bin/bash
 
-DIR=~/bigWigLiftOver
-FILE=~/bigWigToBedGraph
-FILE2=~/hg18ToHg19.over.chain.gz
-FILE3=~/liftOver
-FILE4=~/bedGraphToBigWig 
+DIR=bigWigLiftOver
+FILE=bigWigToBedGraph
+FILE2=hg18ToHg19.over.chain.gz
+FILE3=liftOver
+FILE4=hg19.chrom.sizes
+FILE5=bedGraphToBigWig 
 
 BIGWIG=$(pwd)/$1
 echo Proccesing file:
@@ -42,25 +43,38 @@ fi
 
 gunzip hg18ToHg19.over.chain.gz
 
-#check if liftOver file exists, if not, download from http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64.v287/
+#check if liftOver file exists, if not, download from http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v287/liftOver
 
 if [ ! -f $FILE3 ]
 then
-wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64.v287/liftOver
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v287/liftOver
 fi
 
 chmod 755 ./liftOver
 
 ./liftOver -bedPlus=4 out.bedGraph hg18ToHg19.over.chain out.bedGraph.hg19 unMapped 
 
-#check if bedGraphToBigWig file exists, if not, download from http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64.v287/
+out.bedGraph.hg19 > out.bedGraph.hg19.sort
+
+#check if hg19.chrom.sizes file exists, if not, download from https://genome.ucsc.edu/goldenpath/help/hg19.chrom.sizes
 
 if [ ! -f $FILE4 ]
 then
-wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64.v287/bedGraphToBigWig
+wget https://genome.ucsc.edu/goldenpath/help/hg19.chrom.sizes
+fi
+
+
+#check if bedGraphToBigWig file exists, if not, download from http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v287/bedGraphToBigWig
+
+if [ ! -f $FILE5 ]
+then
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v287/bedGraphToBigWig
 fi
 
 chmod 755 ./bedGraphToBigWig
 
-./bedGraphToBigWig out.bedGraph.hg19 $BIGWIG.hg19
+./bedGraphToBigWig out.bedGraph.hg19.sort hg19.chrom.sizes $BIGWIG.hg19
 
+rm out.bedGraph
+rm out.bedGraph.hg19
+rm out.bedGraph.hg19.sort
